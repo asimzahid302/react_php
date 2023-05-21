84301d297a0127386516a8293b6eb4391697b125
Asim zahid <asimzahid302@gmail.com>
2023-05-21 11:02:54 +0500

    Axios installed and form created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // creating states
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-   <h1>I am react app</h1>
+    <>
+      <div className="form-container">
+        <div className="form-content">
+          <h1>Submit form from react to php</h1>
+          <br />
+
+          <form action="#">
+            <input type="text" name="username" id="username" placeholder="Enter username..." value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <br />
+            <br />
+            <input type="text" name="email" id="email" placeholder="Enter email..." value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <br />
+            <br />
+            <input type="password" name="password" id="password" placeholder="Enter password..." value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <br />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
